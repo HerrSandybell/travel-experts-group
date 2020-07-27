e8c959ddefe2f7718e9c0799831297cf0d795bf8
HerrSandybell <talatrach@gmail.com>
2020-07-27 14:47:05 -0600

    corrected some typos and improved readability of gallery model

---
+++ models/gallery-model.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+// Define Schema
+const packagesSchema =  new mongoose.Schema({
+  Id:                 String,
+  packageName:        String,
+  packageDescription: String,
+  packageStartDate:   Date,
+  packageEndDate:     Date, 
+  packagePrice:       String,
+	imageFile:          String,
+});
+
+// Export model to mongoDB using the above schema
+module.exports = mongoose.model('package', packagesSchema);
